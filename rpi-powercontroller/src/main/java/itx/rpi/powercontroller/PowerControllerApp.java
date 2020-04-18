1bf2b24a62d335791fe82ac4132298f2d884817e
Juraj Veverka <juraj.veverka@inter-net.sk>
2020-04-18 10:56:20 +0200

    added logger dependency

--- rpi-powercontroller/src/main/java/itx/rpi/powercontroller/PowerControllerApp.java
+++ rpi-powercontroller/src/main/java/itx/rpi/powercontroller/PowerControllerApp.java
@@ -1,9 +1,14 @@
 package itx.rpi.powercontroller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PowerControllerApp {
 
-    public static void main(String[] args) {
+    private static final Logger LOG = LoggerFactory.getLogger(PowerControllerApp.class);
 
+    public static void main(String[] args) {
+        LOG.info("Starting PowerController APP ...");
     }
 
 }
