5ff63cf7816e31a08731f5edd0e5a0be4aa8e4c6
Juraj Veverka <juraj.veverka@globallogic.com>
2021-06-02 15:09:46 +0200

    camera info improved

--- rpi-camera/camera-rest.py
+++ rpi-camera/camera-rest.py
@@ -51,7 +51,7 @@ def verify_password(username, password):
 @app.route('/system/info', methods=["GET"])
 @auth.login_required
 def getVersion():
-    version = { "id": config['id'], "type": "camera-rest", "version": "1.2.0", "name": config['name'] }
+    version = { "id": config['id'], "type": "camera-rest", "version": "1.2.0", "name": config['name'], "camera-revision": camera.revision }
     return jsonify(version)
 
 @app.route('/system/capture', methods=["GET"])
