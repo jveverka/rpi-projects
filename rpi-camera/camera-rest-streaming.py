2d82f8e148c74d29879745f685b9a6887fe27098
Juraj Veverka <gergej123@gmail.com>
2021-07-02 00:04:07 +0200

    streaming client updated

--- rpi-camera/camera-rest-streaming.py
+++ rpi-camera/camera-rest-streaming.py
@@ -61,11 +61,14 @@ camera_resolutions = {
   }
 }
 
-resolution=str(camera_resolutions[defaults['resolution']]['image_width']) + 'x' +  str(camera_resolutions[defaults['resolution']]['image_height'])
+resolution = str(camera_resolutions[defaults['resolution']]['image_width']) + 'x' +  str(camera_resolutions[defaults['resolution']]['image_height'])
 
 logging.info('app initialization done.')
 logging.info('resolution: %s', resolution)
 logging.info('framerate : %s', defaults['framerate'])
+
+camera = None
+output = None
 #------------------------------------------------------------------------------
 # INIT SECTION - DONE
 #------------------------------------------------------------------------------
@@ -87,6 +90,7 @@ class StreamingOutput(object):
                 self.condition.notify_all()
             self.buffer.seek(0)
         return self.buffer.write(buf)
+
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -178,6 +182,33 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.send_header('Content-Length', len(content))
             self.end_headers()
             self.wfile.write(content)
+        elif self.path == '/system/config':
+            content_len = int(self.headers.get('content-length'))
+            post_body = self.rfile.read(content_len)
+            body_data = json.loads(post_body)
+            logging.info("resetting camera configuration ...")
+            resolution = str(camera_resolutions[body_data['resolution']]['image_width']) + 'x' +  str(camera_resolutions[body_data['resolution']]['image_height'])
+            rotation = int(camera_rotations[body_data['rotation']])
+            logging.info("framerate  : %s", body_data['framerate'])
+            logging.info("resolution : %s", resolution)
+            logging.info("rotation   : %s", rotation)
+            global camera
+            global output
+            camera.stop_recording()
+            camera.close()
+            logging.info("camera stopped.")
+            camera = picamera.PiCamera(resolution=resolution, framerate=body_data['framerate'])
+            logging.info("camera restarted.")
+            camera.rotation = rotation
+            logging.info("camera set rotation.")
+            camera.start_recording(output, format='mjpeg')
+            logging.info("camera configuration reset.")
+            content = json.dumps(body_data).encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
         else:
             self.send_error(404)
             self.end_headers()
@@ -187,12 +218,13 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
+
 def isAuthorized(authorization, credentials):
     if authorization == None:
-       logging.error("authorization header is missing !");
+       logging.error("authorization header is missing !")
        return False
     if not authorization.startswith( 'Basic ' ):
-       logging.error("authorization Basic is expected !");
+       logging.error("authorization Basic is expected !")
        return False
     decoded = str(base64.b64decode(authorization.split()[1]),'utf-8')
     auth_split = decoded.split(":")
@@ -204,26 +236,30 @@ def isAuthorized(authorization, credentials):
     logging.error("authorization failed !")
     return False
 
-with picamera.PiCamera(resolution=resolution, framerate=defaults['framerate']) as camera:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio_switch_port,GPIO.OUT)
-    logging.info("selected camera: %s", config['camera'])
-    if config['camera'] == 0:
-       GPIO.output(gpio_switch_port, GPIO.LOW)
-    else:
-       GPIO.output(gpio_switch_port, GPIO.HIGH)
-    output = StreamingOutput()
-    camera.rotation = camera_rotations[defaults['rotation']]
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = (config['host'], config['port'])
-        logging.info("starting server at: %s", str(address))
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        logging.info("app shutting down ...")
-        camera.stop_recording()
-        GPIO.cleanup()
-        logging.info("app stopped.")
-        sys.exit(0)
-        pass
+
+
+if __name__ == '__main__':
+   try:
+      camera = picamera.PiCamera(resolution=resolution, framerate=defaults['framerate'])
+      output = StreamingOutput()
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(gpio_switch_port,GPIO.OUT)
+      logging.info("selected camera: %s", config['camera'])
+      if config['camera'] == 0:
+         GPIO.output(gpio_switch_port, GPIO.LOW)
+      else:
+         GPIO.output(gpio_switch_port, GPIO.HIGH)
+      camera.rotation = camera_rotations[defaults['rotation']]
+      camera.start_recording(output, format='mjpeg')
+      address = (config['host'], config['port'])
+      logging.info("starting server at: %s", str(address))
+      server = StreamingServer(address, StreamingHandler)
+      server.serve_forever()
+   except (KeyboardInterrupt, SystemExit):
+      logging.info("app shutting down ...")
+      camera.stop_recording()
+      camera.close()
+      GPIO.cleanup()
+      logging.info("app stopped.")
+      sys.exit(0)
+      pass
