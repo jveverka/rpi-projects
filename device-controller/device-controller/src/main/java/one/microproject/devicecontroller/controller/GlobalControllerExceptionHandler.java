179ca1dc01524010c510b3e6b9df0214ece74420
Juraj Veverka <gergej123@gmail.com>
2021-07-12 21:18:54 +0200

    adding ExpiredJwtException handler

--- device-controller/device-controller/src/main/java/one/microproject/devicecontroller/controller/GlobalControllerExceptionHandler.java
+++ device-controller/device-controller/src/main/java/one/microproject/devicecontroller/controller/GlobalControllerExceptionHandler.java
@@ -6,13 +6,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import io.jsonwebtoken.ExpiredJwtException;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(DeviceException.class)
-    public ResponseEntity<ExceptionResponse> deviceAdminResponse(DeviceException e) {
+    public ResponseEntity<ExceptionResponse> handleDeviceException(DeviceException e) {
         ExceptionResponse response = new ExceptionResponse(HttpStatus.BAD_REQUEST,
+                e.getClass().getCanonicalName(), e.getMessage(), e.getCause().getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ExceptionResponse> handleExpiredJwtException(ExpiredJwtException e) {
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.UNAUTHORIZED,
                 e.getClass().getCanonicalName(), e.getMessage(), e.getCause().getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
