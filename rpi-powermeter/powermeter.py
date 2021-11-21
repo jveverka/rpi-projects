06ae1b61ad86a6b2640796c36bddab532bb47483
Juraj Veverka <gergej123@gmail.com>
2021-11-22 00:09:16 +0100

    added elasticsearch authorization

--- rpi-powermeter/powermeter.py
+++ rpi-powermeter/powermeter.py
@@ -5,6 +5,7 @@ import json
 import sys
 import time
 import logging
+import base64
 import http.client
 
 cf = open(sys.argv[1])
@@ -26,10 +27,13 @@ device_id    = config["device-id"]
 pulse_value  = config["pulse-value"]
 voltage_ac   = config["voltage-ac"]
 cost_kwh     = config["cost-kwh"]
-elastic      = config["data-store"]["elastic"]
-index        = config["data-store"]["index"]
 co2g_per_kwh = config["co2g-per-kwh"]
 max_power_kw = config["max-power-kw"]
+
+elastic      = config["data-store"]["elastic"]
+index        = config["data-store"]["index"]
+elastic_user = config["data-store"]["user"]
+elastic_pass = config["data-store"]["password"]
 
 logging.info('device_id: %s', device_id)
 logging.info('pulse_value: %s', pulse_value)
@@ -41,11 +45,22 @@ logging.info('co2g_per_kwh: %s', co2g_per_kwh)
 logging.info('max-power-kw: %s', max_power_kw)
 logging.info('meter_pin: %s', meter_pin)
 
+
+def get_authorization(username, password):
+    auth_str = username + ":" + password
+    return "Basic " + base64.b64encode(auth_str.encode('ascii')).decode('ascii')
+
+
+elastic_authorization = get_authorization(elastic_user, elastic_pass)
 last_timestamp = 0
+logging.info('elastic_authorization: %s', elastic_authorization)
+elastic_headers = {
+    "Content-type": "application/json",
+    "Authorization": elastic_authorization
+}
 
 def write_to_elastic(device_id, now_timestamp, interval, voltage, consumed, price, power, current, co2_produced):
     now_timestamp = int(now_timestamp * 1000)
-    headers = {"Content-type": "application/json"}
     body = {
         "timestamp": now_timestamp,
         "deviceId": device_id,
@@ -60,9 +75,10 @@ def write_to_elastic(device_id, now_timestamp, interval, voltage, consumed, pric
         }
     }
     connection = http.client.HTTPConnection(elastic)
-    connection.request("POST", index + "/_doc", headers = headers, body = json.dumps(body))
+    connection.request("POST", index + "/_doc", headers = elastic_headers, body = json.dumps(body))
     response = connection.getresponse()
     logging.info("elastic response: %s", response.status)
+
 
 while True:
     GPIO.wait_for_edge(meter_pin, GPIO.FALLING)
