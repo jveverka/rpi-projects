af99ff1b9ce27cefe0e3ad241aa1a74e36b0a7f3
Juraj Veverka <gergej123@gmail.com>
2022-04-02 20:29:19 +0200

    ADS115 fixed

--- rpi-drivers-ng/src/main/java/one/microproject/rpi/hardware/gpio/sensors/sensors/ADS1115.java
+++ rpi-drivers-ng/src/main/java/one/microproject/rpi/hardware/gpio/sensors/sensors/ADS1115.java
@@ -12,6 +12,16 @@ public class ADS1115 implements AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(ADS1115.class);
 
     private static final int ADDRESS = 0x48;
+    private static final int CONVERSION_REGISTER = 0x00;
+    private static final int CONFIG_REGISTER = 0x01;
+    private static final int LO_THRESH_REGISTER = 0x02;
+    private static final int HI_THRESH_REGISTER = 0x03;
+
+    private static final int CONFIG_REGISTER_VALUE_A0 = 0b1100010110000011;
+    private static final int CONFIG_REGISTER_VALUE_A1 = 0b1101010110000011;
+    private static final int CONFIG_REGISTER_VALUE_A2 = 0b1110010110000011;
+    private static final int CONFIG_REGISTER_VALUE_A3 = 0b1111010110000011;
+    private static final double GAIN_PER_BITE = 62.5/1_000_000;
 
     private final I2C i2c;
 
@@ -23,22 +33,35 @@ public class ADS1115 implements AutoCloseable {
         I2CProvider i2CProvider = pi4j.provider("linuxfs-i2c");
         I2CConfig i2cConfig = I2C.newConfigBuilder(pi4j).id("ADS1115").bus(1).device(address).build();
         i2c = i2CProvider.create(i2cConfig);
+        LOG.info("ADS1115 Connected to bus {}. OK.", address);
     }
 
     public double readAIn0() {
-        return 0;
+        return GAIN_PER_BITE * readIn(CONFIG_REGISTER_VALUE_A0);
     }
 
     public double readAIn1() {
-        return 0;
+        return GAIN_PER_BITE * readIn(CONFIG_REGISTER_VALUE_A1);
     }
 
     public double readAIn2() {
-        return 0;
+        return GAIN_PER_BITE * readIn(CONFIG_REGISTER_VALUE_A2);
     }
 
     public double readAIn3() {
-        return 0;
+        return GAIN_PER_BITE * readIn(CONFIG_REGISTER_VALUE_A3);
+    }
+
+    private int readIn(int address) {
+        i2c.writeRegisterWord(CONFIG_REGISTER, address);
+        try {
+            Thread.sleep(25);
+        } catch (InterruptedException e) {
+            LOG.error("Error: ", e);
+        }
+        int result = i2c.readRegisterWord(CONVERSION_REGISTER);
+        LOG.info("readIn: {}, raw {}", address, result);
+        return result;
     }
 
     @Override
--- rpi-drivers-ng/src/main/java/one/microproject/rpi/hardware/gpio/sensors/tests/ADS1115Test.java
+++ rpi-drivers-ng/src/main/java/one/microproject/rpi/hardware/gpio/sensors/tests/ADS1115Test.java
@@ -1,6 +1,7 @@
 package one.microproject.rpi.hardware.gpio.sensors.tests;
 
 import com.pi4j.context.Context;
+import one.microproject.rpi.hardware.gpio.sensors.sensors.ADS1115;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,8 +12,19 @@ public class ADS1115Test {
     private ADS1115Test() {
     }
 
-    public static void test(Context context) {
+    public static void test(Context context) throws Exception {
         LOG.info("ADS1115Test started ...");
+        try (ADS1115 ads1115 = new ADS1115(context)) {
+            for (int i = 0; i < 10; i++) {
+                double aIn0 = ads1115.readAIn0();
+                double aIn1 = ads1115.readAIn1();
+                double aIn2 = ads1115.readAIn2();
+                double aIn3 = ads1115.readAIn3();
+                LOG.info("[{}] Voltages: a0={} V, a1={} V, a2={} V, a3={} V",
+                        i, String.format("%.3f", aIn0), String.format("%.3f", aIn1), String.format("%.3f", aIn2), String.format("%.3f", aIn3));
+                Thread.sleep(500);
+            }
+        }
         LOG.info("ADS1115Test done.");
     }
 
